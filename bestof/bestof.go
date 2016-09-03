f7d33f0f3c3898fbf6f08e754957e2dd736cdeed
Bruce Lee <jangsoo.lee@gmail.com>
2016-09-03 18:08:25 -0400

    adding 502 behavior, fixing construction in test harness

--- bestof/bestof.go
+++ bestof/bestof.go
@@ -12,12 +12,12 @@ import (
 // - #Map of 'backends' to semaphore<empty struct>
 // - #Random number provider
 // - #Ability to insert random number provider for testing purposes
-// - Code for request proxying
-// - Increment and decrement semaphore appropriately during proxy
+// - #Code for request proxying
+// - #Increment and decrement counter appropriately during proxy
 // - If number of backends is zero, return 502
 // - #If number of backends is one, follow next immediately, do not follow algorithm
 // - #N should never be zero or greater than number of backends, default to two or number of backends
-// - Configuration should allow for selection of N, should default to two if not present
+// - #Configuration should allow for selection of N, should default to two if not present
 // - #If N is equal to number of backends, fall back to JSQ, do not go through selection
 // - #If N is less than number of backends, randomly select N backends and choose the least loaded
 
@@ -103,6 +103,9 @@ func NewBalancer(balancees []string, randomInt RandomInt, choices int, next http
 		b.balancees[purl] = 0
 	}
 	b.randomGenerator = randomInt
+	if choices == 0 {
+		choices = 2
+	}
 	b.choices = choices
 	b.next = next
 	return &b
@@ -121,6 +124,12 @@ func (b *Balancer) release(u *url.URL) {
 }
 
 func (b *Balancer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if len(b.keys) == 0 {
+		w.WriteHeader(502)
+		fmt.Fprint(w, "bestofnlb has no balancees. no backend server available to fulfill this request.")
+		return
+		//return 502
+	}
 	var next, _ = b.nextServer()
 	newReq := *req
 	newReq.URL = next
--- server.go
+++ server.go
@@ -11,9 +11,10 @@ import (
 func main() {
 	var fwd, _ = forward.New()
 	var bal = bestof.NewBalancer(
-		[]string{"http://testa:8080", "http://testb:8080", "http://testc:8080"},
+		[]string{}, //"http://testa:8080", "http://testb:8080", "http://testc:8080"},
 		bestof.GoRandom{},
 		2,
+		fwd,
 	)
 	http.HandleFunc("/asdf", bal.ServeHTTP)
 	http.ListenAndServe(":8090", nil)
