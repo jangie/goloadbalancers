437858179551008f32b1785a199fb447c4d859c6
Bruce Lee <jangsoo.lee@gmail.com>
2016-09-04 19:54:29 -0400

    Handle single choice degenerate case

--- bestof/bestof.go
+++ bestof/bestof.go
@@ -8,20 +8,6 @@ import (
 	"sync"
 )
 
-//TODO:
-// - #semaphore concept: See http://www.golangpatterns.info/concurrency/semaphores
-// - #Map of 'backends' to semaphore<empty struct>
-// - #Random number provider
-// - #Ability to insert random number provider for testing purposes
-// - #Code for request proxying
-// - #Increment and decrement counter appropriately during proxy
-// - #If number of backends is zero, return 502
-// - #If number of backends is one, follow next immediately, do not follow algorithm
-// - #N should never be zero or greater than number of backends, default to two or number of backends
-// - #Configuration should allow for selection of N, should default to two if not present
-// - #If N is equal to number of backends, fall back to JSQ, do not go through selection
-// - #If N is less than number of backends, randomly select N backends and choose the least loaded
-
 //ChoiceOfBalancer is a bookkeeping struct
 type ChoiceOfBalancer struct {
 	balancees       map[*url.URL]int
@@ -41,10 +27,6 @@ type ChoiceOfBalancerOptions struct {
 	IsTesting       bool
 }
 
-//constructor must:
-//- set up keys to be keys of balancees Map
-//- set up randomGenerator to choose between 0 and
-
 func (b *ChoiceOfBalancer) nextServer() (*url.URL, error) {
 	b.lock.Lock()
 	defer b.lock.Unlock()
@@ -59,8 +41,8 @@ func (b *ChoiceOfBalancer) nextServer() (*url.URL, error) {
 		}
 	}
 	var normalizedChoices = b.choices
-	//Special case: If choices is <= 0, default to 2
-	if normalizedChoices <= 0 {
+	//Special case: If choices is <= 1, default to 2. 1 choice is effectively a random LB.
+	if normalizedChoices <= 1 {
 		normalizedChoices = 2
 	}
 	//Special case: If choices > number of balancees, default to number of backends
@@ -121,7 +103,8 @@ func NewChoiceOfBalancer(balancees []string, options ChoiceOfBalancerOptions, ne
 	} else {
 		b.randomGenerator = options.RandomGenerator
 	}
-	if options.Choices == 0 {
+	//Zero choices makes for an impossible decision, one choice is effectively a random LB
+	if options.Choices <= 1 {
 		b.choices = 2
 	} else {
 		b.choices = options.Choices
